58bb5e5ef05b94cb5c75f4c32cfca5cbeb59579c
ayush-verma1708 <ayush.verma@s.amity.edu>
2024-09-18 12:20:16 +0530

    Updated icons in sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,18 +1,33 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import clsx from 'clsx';
-import HouseRoundedIcon from '@mui/icons-material/HouseRounded';
-import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
-import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
-import GroupAddOutlinedIcon from '@mui/icons-material/GroupAddOutlined';
-import VideogameAssetOutlinedIcon from '@mui/icons-material/VideogameAssetOutlined';
-import FactCheckOutlinedIcon from '@mui/icons-material/FactCheckOutlined';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import ScoreboardOutlinedIcon from '@mui/icons-material/ScoreboardOutlined';
+import InsightsOutlinedIcon from '@mui/icons-material/InsightsOutlined';
+import DevicesOutlinedIcon from '@mui/icons-material/DevicesOutlined';
+import ChecklistOutlinedIcon from '@mui/icons-material/ChecklistOutlined';
+import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
+import FolderSharedOutlinedIcon from '@mui/icons-material/FolderSharedOutlined';
+import SettingsApplicationsOutlinedIcon from '@mui/icons-material/SettingsApplicationsOutlined';
+import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 
-import ControlPointDuplicateOutlinedIcon from '@mui/icons-material/ControlPointDuplicateOutlined';
-import ControlPointOutlinedIcon from '@mui/icons-material/ControlPointOutlined';
-import AttractionsOutlinedIcon from '@mui/icons-material/AttractionsOutlined';
+// import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+// import FactCheckOutlinedIcon from '@mui/icons-material/FactCheckOutlined';
+// import VideogameAssetOutlinedIcon from '@mui/icons-material/VideogameAssetOutlined';
+// import GroupAddOutlinedIcon from '@mui/icons-material/GroupAddOutlined';
+// import ControlPointDuplicateOutlinedIcon from '@mui/icons-material/ControlPointDuplicateOutlined';
+// import ControlPointOutlinedIcon from '@mui/icons-material/ControlPointOutlined';
+// import AttractionsOutlinedIcon from '@mui/icons-material/AttractionsOutlined';
+
+// import HouseRoundedIcon from '@mui/icons-material/HouseRounded';
+// import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+
+// import ControlPointDuplicateOutlinedIcon from '@mui/icons-material/ControlPointDuplicateOutlined';
+// import ControlPointOutlinedIcon from '@mui/icons-material/ControlPointOutlined';
+// import AttractionsOutlinedIcon from '@mui/icons-material/AttractionsOutlined';
 import {
   Box,
   Drawer,
@@ -46,6 +61,245 @@ const Sidebar = ({ onSelect }) => {
   };
 
   return (
+    // <Drawer
+    //   variant='permanent'
+    //   open={open}
+    //   sx={{
+    //     width: open ? 240 : 60,
+    //     flexShrink: 0,
+    //     backgroundColor: '#333',
+    //     [`& .MuiDrawer-paper`]: {
+    //       width: open ? 240 : 60,
+    //       boxSizing: 'border-box',
+    //       overflowX: 'hidden',
+    //       transition: 'width 0.5s',
+    //     },
+    //   }}
+    // >
+    //   <Box
+    //     sx={{
+    //       display: 'flex',
+    //       flexDirection: 'column',
+    //       height: '100%',
+    //       backgroundColor: '#333',
+    //     }}
+    //   >
+    //     <IconButton
+    //       onClick={toggleDrawer}
+    //       sx={{
+    //         alignSelf: open ? 'flex-end' : 'center',
+    //         margin: open ? '0 8px' : '0',
+    //       }}
+    //     >
+    //       {open ? (
+    //         <ChevronLeftIcon sx={{ color: 'white' }} />
+    //       ) : (
+    //         <MenuIcon sx={{ color: 'white' }} />
+    //       )}
+    //     </IconButton>
+
+    //     <List
+    //       sx={{
+    //         flexGrow: 1,
+    //         flexDirection: 'column',
+    //         justifyContent: 'center',
+    //         alignItems: open ? 'flex-start' : 'center',
+    //       }}
+    //     >
+    //       {hasAccess(['Compliance Team', 'Admin', 'IT Team', 'Auditor']) && (
+    //         <ListItem
+    //           button
+    //           component={Link}
+    //           to='/'
+    //           className={clsx({ active: activePath === '/' })}
+    //           aria-label='Home'
+    //           onClick={() => onSelect('Home')}
+    //           sx={{ width: '100%' }}
+    //         >
+    //           <ListItemIcon>
+    //             <HouseRoundedIcon sx={{ color: 'white' }} />
+    //           </ListItemIcon>
+    //           {open && <ListItemText primary='Home' sx={{ color: 'white' }} />}
+    //         </ListItem>
+    //       )}
+
+    //       {hasAccess(['Compliance Team', 'Executive', 'IT Team']) && (
+    //         <ListItem
+    //           button
+    //           component={Link}
+    //           to='/scoreboard'
+    //           className={clsx({ active: activePath === '/scoreboard' })}
+    //           aria-label='Scoreboard'
+    //           onClick={() => onSelect('Scoreboard')}
+    //           sx={{ width: '100%' }}
+    //         >
+    //           <ListItemIcon>
+    //             <DashboardOutlinedIcon sx={{ color: 'white' }} />
+    //           </ListItemIcon>
+    //           {open && (
+    //             <ListItemText primary='Scoreboard' sx={{ color: 'white' }} />
+    //           )}
+    //         </ListItem>
+    //       )}
+
+    //       {hasAccess(['Compliance Team', 'Executive']) && (
+    //         <ListItem
+    //           button
+    //           component={Link}
+    //           to='/risk-analysis'
+    //           className={clsx({ active: activePath === '/risk-analysis' })}
+    //           aria-label='Risk Analysis'
+    //           onClick={() => onSelect('Risk Analysis')}
+    //           sx={{ width: '100%' }}
+    //         >
+    //           <ListItemIcon>
+    //             <FactCheckOutlinedIcon sx={{ color: 'white' }} />
+    //           </ListItemIcon>
+    //           {open && (
+    //             <ListItemText primary='Risk Analysis' sx={{ color: 'white' }} />
+    //           )}
+    //         </ListItem>
+    //       )}
+
+    //       {hasAccess(['Compliance Team', 'IT Team']) && (
+    //         <ListItem
+    //           button
+    //           component={Link}
+    //           to='/asset-management'
+    //           className={clsx({ active: activePath === '/asset-management' })}
+    //           aria-label='Asset Management'
+    //           onClick={() => onSelect('Asset Management')}
+    //           sx={{ width: '100%' }}
+    //         >
+    //           <ListItemIcon>
+    //             <VideogameAssetOutlinedIcon sx={{ color: 'white' }} />
+    //           </ListItemIcon>
+    //           {open && (
+    //             <ListItemText
+    //               primary='Asset Management'
+    //               sx={{ color: 'white' }}
+    //             />
+    //           )}
+    //         </ListItem>
+    //       )}
+
+    //       {hasAccess(['Compliance Team', 'IT Team', 'Auditor']) && (
+    //         <ListItem
+    //           button
+    //           component={Link}
+    //           to='/list-of-actions'
+    //           className={clsx({ active: activePath === '/list-of-actions' })}
+    //           aria-label='List of Actions'
+    //           onClick={() => onSelect('List of Actions')}
+    //           sx={{ width: '100%' }}
+    //         >
+    //           <ListItemIcon>
+    //             <FactCheckOutlinedIcon sx={{ color: 'white' }} />
+    //           </ListItemIcon>
+    //           {open && (
+    //             <ListItemText primary='M.O.S.' sx={{ color: 'white' }} />
+    //           )}
+    //         </ListItem>
+    //       )}
+
+    //       {hasAccess(['Compliance Team', 'Admin']) && (
+    //         <ListItem
+    //           button
+    //           component={Link}
+    //           to='/user-creation'
+    //           className={clsx({ active: activePath === '/user-creation' })}
+    //           aria-label='User Creation'
+    //           onClick={() => onSelect('User Creation')}
+    //           sx={{ width: '100%' }}
+    //         >
+    //           <ListItemIcon>
+    //             <GroupAddOutlinedIcon sx={{ color: 'white' }} />
+    //           </ListItemIcon>
+    //           {open && (
+    //             <ListItemText primary='User Creation' sx={{ color: 'white' }} />
+    //           )}
+    //         </ListItem>
+    //       )}
+
+    //       {hasAccess(['Compliance Team']) && (
+    //         <ListItem
+    //           button
+    //           component={Link}
+    //           to='/control-families'
+    //           className={clsx({ active: activePath === '/control-families' })}
+    //           aria-label='Control Families'
+    //           onClick={() => onSelect('Control Families')}
+    //           sx={{ width: '100%' }}
+    //         >
+    //           <ListItemIcon>
+    //             <ControlPointDuplicateOutlinedIcon sx={{ color: 'white' }} />
+    //           </ListItemIcon>
+    //           {open && (
+    //             <ListItemText
+    //               primary='Control Families'
+    //               sx={{ color: 'white' }}
+    //             />
+    //           )}
+    //         </ListItem>
+    //       )}
+    //       {hasAccess(['Compliance Team']) && (
+    //         <ListItem
+    //           button
+    //           component={Link}
+    //           to='/controls'
+    //           className={clsx({ active: activePath === '/controls' })}
+    //           aria-label='Controls'
+    //           onClick={() => onSelect('Controls')}
+    //           sx={{ width: '100%' }}
+    //         >
+    //           <ListItemIcon>
+    //             <ControlPointOutlinedIcon sx={{ color: 'white' }} />
+    //           </ListItemIcon>
+    //           {open && (
+    //             <ListItemText primary='Controls' sx={{ color: 'white' }} />
+    //           )}
+    //         </ListItem>
+    //       )}
+
+    //       {hasAccess(['Compliance Team']) && (
+    //         <ListItem
+    //           button
+    //           component={Link}
+    //           to='/actions'
+    //           className={clsx({ active: activePath === '/actions' })}
+    //           aria-label='Actions'
+    //           onClick={() => onSelect('Actions')}
+    //           sx={{ width: '100%' }}
+    //         >
+    //           <ListItemIcon>
+    //             <AttractionsOutlinedIcon sx={{ color: 'white' }} />
+    //           </ListItemIcon>
+    //           {open && (
+    //             <ListItemText primary='Actions' sx={{ color: 'white' }} />
+    //           )}
+    //         </ListItem>
+    //       )}
+
+    //       {/* Settings Page - accessible to all roles */}
+    //       <ListItem
+    //         button
+    //         component={Link}
+    //         to='/settings'
+    //         className={clsx({ active: activePath === '/settings' })}
+    //         aria-label='Settings'
+    //         onClick={() => onSelect('Settings')}
+    //         sx={{ width: '100%' }}
+    //       >
+    //         <ListItemIcon>
+    //           <DashboardOutlinedIcon sx={{ color: 'white' }} />
+    //         </ListItemIcon>
+    //         {open && (
+    //           <ListItemText primary='Settings' sx={{ color: 'white' }} />
+    //         )}
+    //       </ListItem>
+    //     </List>
+    //   </Box>
+    // </Drawer>
     <Drawer
       variant='permanent'
       open={open}
@@ -102,7 +356,7 @@ const Sidebar = ({ onSelect }) => {
               sx={{ width: '100%' }}
             >
               <ListItemIcon>
-                <HouseRoundedIcon sx={{ color: 'white' }} />
+                <HomeOutlinedIcon sx={{ color: 'white' }} /> {/* Updated */}
               </ListItemIcon>
               {open && <ListItemText primary='Home' sx={{ color: 'white' }} />}
             </ListItem>
@@ -119,7 +373,8 @@ const Sidebar = ({ onSelect }) => {
               sx={{ width: '100%' }}
             >
               <ListItemIcon>
-                <DashboardOutlinedIcon sx={{ color: 'white' }} />
+                <ScoreboardOutlinedIcon sx={{ color: 'white' }} />{' '}
+                {/* Updated */}
               </ListItemIcon>
               {open && (
                 <ListItemText primary='Scoreboard' sx={{ color: 'white' }} />
@@ -138,7 +393,7 @@ const Sidebar = ({ onSelect }) => {
               sx={{ width: '100%' }}
             >
               <ListItemIcon>
-                <FactCheckOutlinedIcon sx={{ color: 'white' }} />
+                <InsightsOutlinedIcon sx={{ color: 'white' }} /> {/* Updated */}
               </ListItemIcon>
               {open && (
                 <ListItemText primary='Risk Analysis' sx={{ color: 'white' }} />
@@ -157,7 +412,7 @@ const Sidebar = ({ onSelect }) => {
               sx={{ width: '100%' }}
             >
               <ListItemIcon>
-                <VideogameAssetOutlinedIcon sx={{ color: 'white' }} />
+                <DevicesOutlinedIcon sx={{ color: 'white' }} /> {/* Updated */}
               </ListItemIcon>
               {open && (
                 <ListItemText
@@ -179,7 +434,8 @@ const Sidebar = ({ onSelect }) => {
               sx={{ width: '100%' }}
             >
               <ListItemIcon>
-                <FactCheckOutlinedIcon sx={{ color: 'white' }} />
+                <ChecklistOutlinedIcon sx={{ color: 'white' }} />{' '}
+                {/* Updated */}
               </ListItemIcon>
               {open && (
                 <ListItemText primary='M.O.S.' sx={{ color: 'white' }} />
@@ -198,7 +454,8 @@ const Sidebar = ({ onSelect }) => {
               sx={{ width: '100%' }}
             >
               <ListItemIcon>
-                <GroupAddOutlinedIcon sx={{ color: 'white' }} />
+                <PersonAddAltOutlinedIcon sx={{ color: 'white' }} />{' '}
+                {/* Updated */}
               </ListItemIcon>
               {open && (
                 <ListItemText primary='User Creation' sx={{ color: 'white' }} />
@@ -217,7 +474,8 @@ const Sidebar = ({ onSelect }) => {
               sx={{ width: '100%' }}
             >
               <ListItemIcon>
-                <ControlPointDuplicateOutlinedIcon sx={{ color: 'white' }} />
+                <FolderSharedOutlinedIcon sx={{ color: 'white' }} />{' '}
+                {/* Updated */}
               </ListItemIcon>
               {open && (
                 <ListItemText
@@ -227,6 +485,7 @@ const Sidebar = ({ onSelect }) => {
               )}
             </ListItem>
           )}
+
           {hasAccess(['Compliance Team']) && (
             <ListItem
               button
@@ -238,7 +497,8 @@ const Sidebar = ({ onSelect }) => {
               sx={{ width: '100%' }}
             >
               <ListItemIcon>
-                <ControlPointOutlinedIcon sx={{ color: 'white' }} />
+                <SettingsApplicationsOutlinedIcon sx={{ color: 'white' }} />{' '}
+                {/* Updated */}
               </ListItemIcon>
               {open && (
                 <ListItemText primary='Controls' sx={{ color: 'white' }} />
@@ -257,7 +517,8 @@ const Sidebar = ({ onSelect }) => {
               sx={{ width: '100%' }}
             >
               <ListItemIcon>
-                <AttractionsOutlinedIcon sx={{ color: 'white' }} />
+                <ManageAccountsOutlinedIcon sx={{ color: 'white' }} />{' '}
+                {/* Updated */}
               </ListItemIcon>
               {open && (
                 <ListItemText primary='Actions' sx={{ color: 'white' }} />
@@ -276,7 +537,7 @@ const Sidebar = ({ onSelect }) => {
             sx={{ width: '100%' }}
           >
             <ListItemIcon>
-              <DashboardOutlinedIcon sx={{ color: 'white' }} />
+              <SettingsOutlinedIcon sx={{ color: 'white' }} /> {/* Updated */}
             </ListItemIcon>
             {open && (
               <ListItemText primary='Settings' sx={{ color: 'white' }} />
